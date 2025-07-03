d560e053e0ea58141f1362b876a7b96eeaaed1b2
Hikaru Tomizawa <tomizawa.licht@gmail.com>
2025-07-03 17:46:15 +0900

    xgboost train-val pipeline

--- src/fraud_detection/models/xgboost_model.py
+++ src/fraud_detection/models/xgboost_model.py
@@ -1,5 +1,5 @@
 from logging import getLogger
-
+from typing import Dict, Any
 import pandas as pd
 from xgboost import XGBClassifier
 
@@ -15,28 +15,34 @@ logger = setup_logger(logger, level="DEBUG")
 class XGBoostModel(BaseModel):
     """
     XGBoost classifier class.
-    Runs based on YAML configuration.
-    Capsule incremental training logic.
+        - Runs based on YAML configuration
+        - Capsules incremental training logic
+        - Handles overfitting with early stopping
+        - Handles imbalanced data with scale_pos_weight
     """
 
-    def __init__(self, model_params: dict):
+    def __init__(self, model_params: Dict[str, Any]):
         super().__init__(model_params)
         self.model = XGBClassifier(**self.model_params)
         self._is_trained = False  # Flag to manage incremental training
 
     def fit(self, X_train: pd.DataFrame, y_train: pd.Series, **kwargs):
         logger.info("Training XGBoost model...")
-
-        fit_params = kwargs
-        if self._is_trained:
-            fit_params["xgb_model"] = self.model
-
-        self.model.fit(X_train, y_train, **fit_params)
+        
+        model_to_train = self.model if not self._is_trained else None
+        
+        fit_params = kwargs.copy()
+        
+        self.model.fit(
+            X_train,
+            y_train,
+            xgb_model=model_to_train,
+            **fit_params
+        )
         self._is_trained = True
 
         logger.info("Training step completed.")
 
-        pass
 
     def predict(self, X_test: pd.DataFrame, **kwargs):
         logger.info("Predicting with XGBoost model...")
@@ -47,14 +53,15 @@ class XGBoostModel(BaseModel):
 
         pass
 
-    def predict_proba(self, X_test, **kwargs):
+    def predict_proba(self, X_test):
+        if self.model is None or not self._is_trained:
+            raise RuntimeError("Model is not trained yet. Call `fit()` at least once.")
+        
         logger.info("Predicting probability with XGBoost model...")
-
-        # TODO: implement prediction logic
-
+        proba = self.model.predict_proba(X_test)[:, 1]
         logger.info("Prediction completed.")
-
-        pass
+        
+        return proba
 
     def save_model(self, path):
         logger.info("Saving XGBoost model...")
--- src/scripts/train_models.py
+++ src/scripts/train_models.py
@@ -2,7 +2,7 @@ import gc
 import importlib
 import math
 from logging import getLogger
-
+from sklearn.metrics import precision_recall_curve
 import numpy as np
 import pandas as pd
 
@@ -20,7 +20,7 @@ CHUNK_SIZE = 10000  # Chunk size for loading data.
 
 
 # ========== Functions ==========
-def import_class(class_path: str):
+def _import_class(class_path: str):
     """
     Dynamically import a class object from import path (string)
 
@@ -34,7 +34,7 @@ def import_class(class_path: str):
     return getattr(module, class_name)
 
 
-def optimize_dtype(df: pd.DataFrame) -> pd.DataFrame:
+def _optimize_dtype(df: pd.DataFrame) -> pd.DataFrame:
     """
     Optimize data types and reduce memory overhead
     """
@@ -64,22 +64,48 @@ def optimize_dtype(df: pd.DataFrame) -> pd.DataFrame:
 
     return df
 
+def _calc_scale_pos_weight(table_name: str, train_rows: int, target: str, engine: object) -> float:
+    logger.info("Calculating scale_pos_weight for training data...")
+    
+    count_query = f"""
+        WITH training_data AS (
+            SELECT {target} FROM {table_name} ORDER BY trans_ts LIMIT {train_rows}
+        )
+        SELECT {target}, COUNT(*) as count FROM training_data GROUP BY {target}
+    """
+    
+    class_counts = pd.read_sql(count_query, engine).set_index(target)['count']
+    neg_count = class_counts.get(0, 0)
+    pos_count = class_counts.get(1, 1)
+    
+    if pos_count == 0:
+        logger.warinig("No positive samples in training data. Setting scale_pos_weight to 1.")
+        return 1
+    
+    scale_pos_weight = neg_count / pos_count
+    logger.info(f"scale_pos_weight: {scale_pos_weight:.3f}")
+    
+    return scale_pos_weight
+    
 
 def train(split_ratio: float = 0.8):
     """
     Load model config YAML and perform training
     """
 
-    config = load_config(root="models")
+    models_config = load_config(root="models")
+    data_config = load_config(root="data")
     engine = get_db_engine()
     table_name = "feature_transactions"
-
-    # Step 1: Train-val split
+    target = data_config["target_column"]
+        
     total_rows = pd.read_sql(f"SELECT COUNT(*) FROM {table_name}", engine).iloc[0, 0]
     train_rows = int(total_rows * split_ratio)
-
+    val_rows = total_rows - train_rows
+    
     total_chunks = math.ceil(total_rows / CHUNK_SIZE)
     train_chunks = math.ceil(train_rows / CHUNK_SIZE)
+
 
     logger.info(
         f"Total chunks: {total_chunks}, Train chunks: {train_chunks}, Validation chunks: {total_chunks - train_chunks}"
@@ -88,15 +114,43 @@ def train(split_ratio: float = 0.8):
         f"Total rows: {total_rows}, Train rows: {train_rows}, Validation rows: {total_rows - train_rows}"
     )
 
+    # Step 1: Train-val split and calc scale_pos_weight
+    logger.info("--- Preparing Data ---")
+    val_sample_size = 20000
+    val_sample_query = f"""
+        SELECT * FROM {table_name} ORDER BY RANDOM() OFFSET {train_rows} LIMIT {val_sample_size}
+    """
+    
+    logger.info(f"Loading validation sample ({val_sample_size} rows) for early stopping...")
+    
+    val_sample_df = pd.read_sql(val_sample_query, engine, chunksize = CHUNK_SIZE, parse_dates=["trans_ts"])
+    val_sample_df = _optimize_dtype(val_sample_df)
+    
+    # The rest of the val data will be loaded via iterator for final evaluation
+    val_query = f"SELECT * FROM {table_name} ORDER BY trans_ts OFFSET {train_rows}"
+    val_iterator = pd.read_sql(
+        val_query,
+        engine,
+        chunksize = CHUNK_SIZE,
+        parse_dates=["trans_ts"]
+        )
+    
     # Step 2: Instantiate learning model
+    scale_pos_weight = _calc_scale_pos_weight(table_name, train_rows, target, engine)
+
     models_to_train = {}
-    for model_name, model_config in config.items():
+    for model_name, model_config in models_config.items():
 
         # Load`run: true` models
         if model_config.get("run", False):
             logger.info(f"Preparing model: '{model_name}'...")
-            ModelClass = import_class(model_config["class_path"])
-            models_to_train[model_name] = ModelClass(model_config["model_params"])
+            ModelClass = _import_class(model_config["class_path"])
+            
+            params = model_config['model_params']
+            if 'xgboost' in model_name.lower():
+                params['scale_pos_weight'] = scale_pos_weight
+                
+            models_to_train[model_name] = ModelClass(model_params=params)
         else:
             logger.info(f"Skipping model: '{model_name}'...")
             continue
@@ -118,7 +172,7 @@ def train(split_ratio: float = 0.8):
 
         # 3.1. Optimize data types
         mem_before = chunk_df.memory_usage(deep=True).sum() / 1024**2
-        chunk_df = optimize_dtype(chunk_df)
+        chunk_df = _optimize_dtype(chunk_df)
         mem_after = chunk_df.memory_usage(deep=True).sum() / 1024**2
         logger.info(
             f"Memory usage optimized: {mem_before:.2f} MB -> {mem_after:.2f} MB"
@@ -126,12 +180,24 @@ def train(split_ratio: float = 0.8):
 
         # 3.2. Train models
         for model_name, model in models_to_train.items():
+            model_config = models_config[model_name]
+            if not model_config.get('run', False):
+                continue
+            
             features = model_config["features"]
-            target = model_config["target"]
+            X_val_sample = val_sample_df[features].copy()
+            y_val_sample = val_sample_df[target].copy()
+            
+            fit_params = model_config.get('fit_params', {})
+
+            # Set evaluation dataset
+            fit_params['eval_set'] = [(X_val_sample, y_val_sample)]
+            
+            # Set train dataset
             X_chunk = chunk_df[features].copy()
             y_chunk = chunk_df[target].copy()
 
-            model.fit(X_chunk, y_chunk)
+            model.fit(X_chunk, y_chunk, **fit_params)
 
         del chunk_df, X_chunk, y_chunk
         gc.collect()  # explicit garbage collection
@@ -139,28 +205,19 @@ def train(split_ratio: float = 0.8):
     logger.info("--- Training Phase Completed ---")
 
     # Step 4: Validateion phase
-    val_query = f"SELECT * FROM {table_name} ORDER BY trans_ts OFFSET {train_rows}"
-    val_iterator = pd.read_sql_query(
-        val_query, engine, chunksize=CHUNK_SIZE, parse_dates=["trans_ts"]
-    )
-
     logger.info("--- Starting Validation Phase ---")
     all_preds = []
     all_trues = []
     for i, chunk_df in enumerate(val_iterator):
         logger.info(f"Validating on chunk {i+1}/{total_chunks - train_chunks}")
 
-        mem_before = chunk_df.memory_usage(deep=True).sum() / 1024**2
-        chunk_df = optimize_dtype(chunk_df)
-        mem_after = chunk_df.memory_usage(deep=True).sum() / 1024**2
-        logger.info(
-            f"Memory usage optimized: {mem_before:.2f} MB -> {mem_after:.2f} MB"
-        )
-
-        X_val_chunk = chunk_df[features].copy()
-        y_val_chunk = chunk_df[target].copy()
+        chunk_df = _optimize_dtype(chunk_df)
+        y_val_chunk = chunk_df[target].copy()     
 
         for model_name, model in models_to_train.items():
+            features = models_config[model_name]["features"]
+            X_val_chunk = chunk_df[features].copy()
+            
             preds = model.predict_proba(X_val_chunk)
             all_preds.extend(preds)
             all_trues.extend(y_val_chunk)
@@ -169,8 +226,21 @@ def train(split_ratio: float = 0.8):
         gc.collect()  # explicit garbage collection
 
     # TODO: Evaluating logic
-    print()
-
+    # --- Step 5: Evaluate final model ---
+    logger.info("--- Final Evaluation Results ---")
+    y_true = np.array(all_trues)
+    
+    for model_name, y_pred_proba in all_preds.items():
+        y_pred_proba = np.array(y_pred_proba)
+        
+        pr_auc = precision_recall_curve(y_true, y_pred_proba)
+        # Placeholder for Precision@K, which should be in evaluation module
+        # p_at_5 = precision_at_k(y_true, y_pred_proba, k_percent=5.0)
+        
+        logger.info(f"Model: {model_name}")
+        logger.info(f"  - PR AUC: {pr_auc:.4f}")
+        # logger.info(f"  - Precision@5%: {p_at_5:.4f}") ... 
+    
     logger.info("--- Validation Phase Completed ---")
 
 
